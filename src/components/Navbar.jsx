ee85573d60be81a451cf81c590ce64d6a8b9b157
reh13597 <alexlguo18@gmail.com>
2025-06-13 12:54:17 -0700

    Changed package.json to try and fix gh pages deployment issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import instagramIcon from '../assets/icons/instagram.svg';
 import facebookIcon from '../assets/icons/facebook.svg';
@@ -9,7 +9,7 @@ export default function Navbar() {
     const location = useLocation();
     const pathName = location.pathname;
     const [isVisible, setIsVisible] = useState(false)
-    
+
     useEffect(() => {
         setTimeout(() => setIsVisible(true), 500)
     }, [])
@@ -18,22 +18,22 @@ export default function Navbar() {
         <div className={`navbar bg-base-300 shadow-sm transition ${isVisible ? 'opacity-100 translate-y-0 delay-500' : 'opacity-0 translate-y-5'}`}>
             <div className="navbar-start">
                 <div className="flex-1">
-                    <a href="/" className="btn text-2xl transition duration-300 hover:scale-110 hover:text-primary">Alex Guo</a>
+                    <Link to="/" className="btn btn-primary btn-ghost text-2xl transition duration-300 hover:scale-110">Alex Guo</Link>
                 </div>
             </div>
             <div className="navbar-center">
                 <div role="tablist" className="flex-auto tabs tabs-border tabs-xl text-primary">
-                    <a role="tab" href="/" className={`tab transition duration-300 hover:scale-110 ${pathName === '/' ? 'tab tab-active' : ''}`}>Home</a>
-                    <a role="tab" href="/reh13597.github.io/coding" className={`tab transition duration-300 hover:scale-110 ${pathName === '/coding' ? 'tab tab-active' : ''}`}>Coding</a>
-                    <a role="tab" href="/reh13597.github.io/contact" className={`tab transition duration-300 hover:scale-110 ${pathName === '/contact' ? 'tab tab-active' : ''}`}>Contact</a>
+                    <Link role="tab" to="/" className={`tab transition duration-300 hover:scale-120 ${pathName === '/' ? 'tab tab-active' : ''}`}>Home</Link>
+                    <Link role="tab" to="/coding" className={`tab transition duration-300 hover:scale-120 ${pathName === '/coding' ? 'tab tab-active' : ''}`}>Coding</Link>
+                    <Link role="tab" to="/contact" className={`tab transition duration-300 hover:scale-120 ${pathName === '/contact' ? 'tab tab-active' : ''}`}>Contact</Link>
                 </div>
             </div>
             <div className="navbar-end">
                 <div className="flex-none">
-                    <a target="_blank" href="https://www.instagram.com/alex_guo_888/" className="btn transition duration-300 hover:scale-120"><img src={instagramIcon} alt="Instagram Hyperlink" className="h-6 w-6" /></a>
-                    <a target="_blank" href="https://www.facebook.com/alex.guo.334839/"className="btn transition duration-300 hover:scale-120"><img src={facebookIcon} alt="Facebook Hyperlink" className="h-6 w-6" /></a>
-                    <a target="_blank" href="https://github.com/reh13597"className="btn transition duration-300 hover:scale-120"><img src={githubIcon} alt="GitHub Hyperlink" className="h-6 w-6" /></a>
-                    <a target="_blank" href="https://www.linkedin.com/in/alex-guo-3088461ba/"className="btn transition duration-300 hover:scale-120"><img src={linkedinIcon} alt="LinkedIn Hyperlink" className="h-6 w-6" /></a>
+                    <a target="_blank" href="https://www.instagram.com/alex_guo_888/" className="btn btn-primary btn-ghost transition duration-300 hover:scale-110"><img src={instagramIcon} alt="Instagram Hyperlink" className="h-6 w-6" /></a>
+                    <a target="_blank" href="https://www.facebook.com/alex.guo.334839/"className="btn btn-primary btn-ghost transition duration-300 hover:scale-110"><img src={facebookIcon} alt="Facebook Hyperlink" className="h-6 w-6" /></a>
+                    <a target="_blank" href="https://github.com/reh13597"className="btn btn-primary btn-ghost transition duration-300 hover:scale-110"><img src={githubIcon} alt="GitHub Hyperlink" className="h-6 w-6" /></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/alex-guo-3088461ba/"className="btn btn-primary btn-ghost transition duration-300 hover:scale-110"><img src={linkedinIcon} alt="LinkedIn Hyperlink" className="h-6 w-6" /></a>
                 </div>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,22 +11,22 @@ export default function Home() {
 
     return(
         <div className="grid grid-cols-3">
-            <div className={`flex justify-center mt-15 transition ${isVisible ? 'opacity-100 translate-y-0 delay-1500' : 'opacity-0 translate-y-5'}`}>
-                <img src={pic1} alt="Picture of Alex Guo" className="h-110 w-80 rounded-4xl transition duration-300 hover:scale-120"></img>
+            <div className={`flex justify-center mt-30 transition ${isVisible ? 'opacity-100 translate-y-0 delay-1500' : 'opacity-0 translate-y-5'}`}>
+                <img src={pic1} alt="Picture of Alex Guo" className="h-130 w-100 rounded-4xl transition duration-300 hover:scale-120"></img>
             </div>
-            <div className={`text-center mt-24 transition ${isVisible ? 'opacity-100 translate-y-0 delay-1000' : 'opacity-0 translate-y-5'}`}>
-                <p className="text-4xl mt-1 transition duration-300 hover:scale-120">
+            <div className={`text-center mt-45 transition ${isVisible ? 'opacity-100 translate-y-0 delay-1000' : 'opacity-0 translate-y-5'}`}>
+                <div className="transition duration-300 hover:scale-130">
                     <span className="text-8xl">Hi! I'm </span>
                     <span className="text-8xl text-primary">Alex</span>
                     <span className="text-8xl">. </span>
-                    A third year CS student at SFU who is passionate about building clean, scalable software. Driven by curiosity, I'm always ready to learn something new!</p>
+                    <p className="text-4xl mt-5">A third year CS student at SFU who is passionate about building clean, scalable software.</p>
+                    <p className="text-4xl mt-5">Driven by curiosity, I'm always ready to learn something new!</p>
+                </div>
             </div>
-            <div className={`flex justify-center mt-15 transition ${isVisible ? 'opacity-100 translate-y-0 delay-1500' : 'opacity-0 translate-y-5'}`}>
-                <img src={pic2} alt="Picture of Alex Guo" className="h-110 w-80 rounded-4xl transition duration-300 hover:scale-120"></img>
+            <div className={`flex justify-center mt-30 transition ${isVisible ? 'opacity-100 translate-y-0 delay-1500' : 'opacity-0 translate-y-5'}`}>
+                <img src={pic2} alt="Picture of Alex Guo" className="h-130 w-100 rounded-4xl transition duration-300 hover:scale-120"></img>
             </div>
         </div>
     );
 }
 
-// WRITE OUT MY BIOGRAPHY FIRST
-// CURRENTLY ON NEW BIO BRANCH
