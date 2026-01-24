c48ab2c1d32279bb6594e7d4a183cfd5ab0ed2d4
reh13597 <alexlguo18@gmail.com>
2026-01-24 11:22:33 -0800

    added text rotation animations to home section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,39 @@ export default function Home() {
                 <div className="mockup-code bg-base-200">
                     <pre data-prefix="$" className="bg-primary text-black text-2xl"><code className="-mx-1">Hi, I'm Alex Guo!</code></pre>
                     <pre data-prefix=">" className="text-xl"><code>Third year CS student @ SFU 🍁</code></pre>
-                    <pre data-prefix=">" className="text-xl"><code>Full-Stack Development 💻, AI/ML 🤖, & Data Networking 🌐</code></pre>
-                    <pre data-prefix=">" className="text-xl"><code>Skiing ⛷️, Hiking ⛰️, Gym 💪, Food 🍕, Anime 📺, Video Games 🎮</code></pre>
+                    <pre data-prefix=">" className="text-xl">
+                        <code>
+                            <span>
+                                I am passionate about
+                                <span className="text-rotate [--items:5] [--duration:20000ms]">
+                                    <span>
+                                        <span className="px-3">Full-Stack Development 💻</span>
+                                        <span className="px-3">AI/ML 🤖</span>
+                                        <span className="px-3">DevOps 🚀</span>
+                                        <span className="px-3">QA 🔍</span>
+                                        <span className="px-3">Data Networking 🌐</span>
+                                    </span>
+                                </span>
+                            </span>
+                        </code>
+                    </pre>
+                    <pre data-prefix=">" className="text-xl">
+                        <code>
+                            <span>
+                                I go crazy for
+                                <span className="text-rotate [--items:6] [--duration:18000ms]">
+                                    <span>
+                                        <span className="px-3">Skiing ⛷️</span>
+                                        <span className="px-3">Hiking ⛰️</span>
+                                        <span className="px-3">Lifting Weights 💪</span>
+                                        <span className="px-3">Food 🍕</span>
+                                        <span className="px-3">Anime 📺</span>
+                                        <span className="px-3">Video Games 🎮</span>
+                                    </span>
+                                </span>
+                            </span>
+                        </code>
+                    </pre>
                 </div>
             </figure>
 
